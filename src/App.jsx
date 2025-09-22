70048570786f509808e4bf01aa4770f09346bc07
Bhuma Naga Pavan <bhumanagapavan@gmail.com>
2025-09-22 11:54:48 +0530

    Tailwind COnfiguration Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import "./App.css";
-
 function App() {
   return (
     <Router>
