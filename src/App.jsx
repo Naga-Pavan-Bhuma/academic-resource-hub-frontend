301f7c4abf4dc4c650534182909da393019d2fb8
Anirudh-654 <anirudhluky654@gmail.com>
2025-09-24 12:32:55 +0530

    StudentDashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import StudentDashboard from "./pages/StudentDashboard"; 
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,10 @@ function App() {
         {/* Landing page for everyone before login */}
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+
+        {/* Student Dashboard */}
+        <Route path="/student" element={<StudentDashboard />} />
+
         {/* Later you can add protected routes here */}
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
